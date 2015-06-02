102148d2620c2734962182c1697553f6782beab4
Alexander Wilden <alexander.wilden@aixigo.de>
2015-06-02 12:56:46 +0200

    Fixed submodule issues.

--- var/static/laxar_application_dependencies.js
+++ var/static/laxar_application_dependencies.js
@@ -2,7 +2,6 @@ define( [
    'laxar-uikit/controls/i18n/ax-i18n-control',
    'finder_demo_utilities/finder_demo_utilities',
    'laxar-application/includes/widgets/finder-demo/search-box-widget/search-box-widget',
-   'laxar-application/includes/widgets/laxarjs/ax-developer-tools-widget/ax-developer-tools-widget',
    'laxar-application/includes/widgets/finder-demo/omdb-search-widget/omdb-search-widget',
    'laxar-application/includes/widgets/finder-demo/wikipedia-search-widget/wikipedia-search-widget',
    'laxar-application/includes/widgets/finder-demo/music-brainz-search-widget/music-brainz-search-widget',
@@ -12,6 +11,6 @@ define( [
 
    var modules = [].slice.call( arguments );
    return {
-      'angular': modules.slice( 0, 8 )
+      'angular': modules.slice( 0, 7 )
    };
 } );
