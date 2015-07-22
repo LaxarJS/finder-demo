9a4699c78de473ae3464a11957adeea56bb4f004
Alexander Wilden <alexander.wilden@aixigo.de>
2015-07-22 10:51:21 +0200

    Finished forecast chart.

--- includes/widgets/finder-demo/open-weather-map-widget/weather-chart-directive.js
+++ includes/widgets/finder-demo/open-weather-map-widget/weather-chart-directive.js
@@ -24,14 +24,25 @@ define( [
          },
          link: function( scope, element ) {
 
-            scope.$watch( 'weatherData', function() {
-               if( !scope.weatherData || scope.weatherData.length === 0 ) {
-                  element.css( 'display', 'none' );
+            scope.$watch( 'weatherData', function( weatherData ) {
+               if( !weatherData || weatherData.length === 0 ) {
+                  return;
                }
-               else {
-                  element.css( 'display', '' );
-                  renderChart();
-               }
+
+               renderChart( weatherData.map( function( entry ) {
+                  var clone = ax.object.deepClone( entry );
+                  clone.momentDate = moment( clone.dt_txt );
+                  clone.shortFormattedTime = clone.momentDate.format( 'HH:mm' );
+                  return clone;
+               } ) );
+            } );
+
+            var languageTag = ui.i18n.languageTagFromScope( scope.$parent );
+            var decimalRules = ui.i18n.numberFormatForLanguageTag( languageTag );
+            var formatString = ax.i18n.localizer( languageTag ).format;
+            var formatDecimal = ui.formatter.create( 'decimal', {
+               decimalSeparator: decimalRules.d,
+               groupingSeparator: decimalRules.g
             } );
 
             // some hardcoded constants
@@ -40,29 +51,27 @@ define( [
             var innerTopOffset = 100;
             var rainBarWidth = 20;
 
+            var svg = d3.select( element[ 0 ] );
+            var svgHeight = svg.attr( 'height' );
+            var contentBoxHeight = svgHeight - 2 * borderMargin;
+            var contentBoxWidth;
+            var svgWidth;
+
+            // A group to add some margins for the complete chart contents
+            var contentBox = svg.append( 'svg:g' )
+               .attr( 'transform', translate( borderMargin, borderMargin ) );
+
+            // The box where all curves and bars are rendered
+            var dataBox = contentBox.append( 'svg:g' )
+               .attr( 'class', 'curves' )
+               .attr( 'transform', 'translate(0,' + innerTopOffset + ')' );
+
             //////////////////////////////////////////////////////////////////////////////////////////////////
 
-            var svg = d3.select( element[ 0 ] );
-            function renderChart() {
-               var languageTag = ui.i18n.languageTagFromScope( scope.$parent );
-               var decimalRules = ui.i18n.numberFormatForLanguageTag( languageTag );
-               var formatString = ax.i18n.localizer( languageTag ).format;
-               var formatDecimal = ui.formatter.create( 'decimal', {
-                  decimalSeparator: decimalRules.d,
-                  groupingSeparator: decimalRules.g
-               } );
+            function renderChart( weatherData ) {
 
-               svg.html('');
-               var svgHeight = svg.attr( 'height' );
-               var contentBoxWidth = scope.weatherData.length * timePeriodWidth;
-               var svgWidth = contentBoxWidth + 2 * borderMargin;
-               var contentBoxHeight = svgHeight - 2 * borderMargin;
-
-               var weatherData = scope.weatherData.map( function( entry ) {
-                  entry.momentDate = moment( entry.dt_txt );
-                  entry.shortFormattedTime = entry.momentDate.format( 'HH:mm' );
-                  return entry;
-               } );
+               contentBoxWidth = weatherData.length * timePeriodWidth;
+               svgWidth = contentBoxWidth + 2 * borderMargin;
 
                var days = weatherData.reduce( function( acc, entry, index ) {
                   if( index === 0 || entry.momentDate.hour() === 0 ) {
@@ -74,22 +83,14 @@ define( [
                   return acc;
                }, [] );
 
-               function entryX( entry, index ) {
-                  return timePeriodWidth / 2 + index * timePeriodWidth;
-               }
-
                svg.attr( 'width', svgWidth );
-
-               var contentBox = svg.append( 'svg:g' )
-                  .attr( 'transform', translate( borderMargin, borderMargin ) );
 
                // Render vertical rulers between two 3-hour periods
                var lineColumnBorder = contentBox.selectAll( 'line.column-border' )
                   // Add a dummy entry to draw the last vertical line
                   .data( weatherData.concat( { momentDate: { hour: function() { return 0; } } } ) );
 
-               lineColumnBorder.enter()
-                  .append( 'svg:line' )
+               lineColumnBorder.enter().append( 'svg:line' )
                   .attr( 'x1', lineX )
                   .attr( 'x2', lineX )
                   .attr( 'y1', newDaySwitch.bind( null, 0, 50 ) )
@@ -99,29 +100,60 @@ define( [
 
                // Render the day name every time a new day starts
                var groupDayName = contentBox.selectAll( 'g.day-name' ).data( days );
-               groupDayName.enter()
-                  .append( 'svg:g' )
+               groupDayName.enter().append( 'svg:g' )
                   .attr( 'class', 'day-name' )
                   .attr( 'transform', function( day ) {
                      return translate( day.x, 20 );
                   } )
                   .append( 'svg:text' )
-                  .text( prop( 'text' ) );
+                  .text( propertyAccessor( 'text' ) );
                groupDayName.exit().remove();
 
+               renderColumnHeaders( weatherData );
 
+               renderTemperatureLine( weatherData );
+
+               renderRainBarChart( weatherData );
+            }
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+
+            function renderColumnHeaders( weatherData ) {
+               // UPDATE
+               contentBox.selectAll( 'g.top-data' )
+                  .transition()
+                  .duration( 250 )
+                  .style( 'opacity', 0 )
+                  .transition()
+                  .call( function() {
+                     contentBox.selectAll( 'g.top-data' ).data( weatherData );
+                  } )
+                  .each( 'end', function( entry ) {
+                     var groupTopData = d3.select( this );
+                     groupTopData.select( 'text.day-time' )
+                        .text( propertyAccessor( 'shortFormattedTime' ) );
+                     groupTopData.select( 'text.weather-icon' )
+                        .text( scope.iconCodes[ entry.weather[ 0 ].icon ] );
+                     groupTopData.select( 'text.temperature' )
+                        .text( Math.round( entry.main.temp ) + ' °C' );
+                  } )
+                  .transition()
+                  .duration( 250 )
+                  .style( 'opacity', 1 );
+
+               // ENTER
                var groupTopData = contentBox.selectAll( 'g.top-data' ).data( weatherData );
                var groupTopDataElement = groupTopData.enter().append( 'svg:g' )
-                  .attr( 'class', 'day-time' )
+                  .attr( 'class', 'top-data' )
                   .attr( 'transform', function( entry, index ) {
                      return 'translate(' + entryX( entry, index ) + ',50)';
                   } );
-               groupTopData.exit().remove();
 
                groupTopDataElement
                   .append( 'svg:text' )
+                  .attr( 'class', 'day-time' )
                   .attr( 'text-anchor', 'middle' )
-                  .text( prop( 'shortFormattedTime' ) );
+                  .text( propertyAccessor( 'shortFormattedTime' ) );
                groupTopDataElement
                   .append( 'svg:text' )
                   .attr( 'class', 'weather-icon' )
@@ -138,7 +170,13 @@ define( [
                      return Math.round( entry.main.temp ) + ' °C';
                   } );
 
+               // EXIT
+               groupTopData.exit().remove();
+            }
 
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+
+            function renderTemperatureLine( weatherData ) {
                var temperatureLine = ( function() {
                   var temperatureY = d3.scale.linear()
                      .range( [ contentBoxHeight - innerTopOffset, 0 ] )
@@ -153,22 +191,51 @@ define( [
                      .y( function( entry ) { return temperatureY( entry.main.temp ); } );
                } )();
 
-               var dataBox = contentBox.append( 'svg:g' )
-                  .attr( 'class', 'curves' )
-                  .attr( 'transform', 'translate(0,' + innerTopOffset + ')' );
+               var temperaturePath = dataBox.selectAll( 'path.temperature-curve' ).data( [ weatherData ] );
 
-               dataBox.selectAll( 'path.temperature-curve' ).remove();
-               dataBox.append( 'svg:path' )
-                  .attr( 'd', temperatureLine( weatherData ) )
-                  .attr( 'class', 'temperature-curve' );
+               // UPDATE
+               temperaturePath
+                  .transition()
+                  .duration( 500 )
+                  .attr( 'd', temperatureLine );
 
+               // ENTER
+               temperaturePath.enter().append( 'svg:path' )
+                  .attr( 'class', 'temperature-curve' )
+                  .attr( 'd', temperatureLine );
+
+               // EXIT
+               temperaturePath.exit().remove();
+            }
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+
+            function renderRainBarChart( weatherData ) {
+               var rain = propertyAccessor( 'rain.3h', 0 );
                var rainY = d3.scale.linear()
                   .range( [ contentBoxHeight - innerTopOffset, (contentBoxHeight - innerTopOffset) / 2 ] )
                   .domain( [ 0, d3.max( weatherData, rain ) ] );
 
                var rectRainBar = dataBox.selectAll( 'rect.rain-bar' ).data( weatherData );
-               rectRainBar.enter()
-                  .append( 'svg:rect' )
+
+               // UPDATE
+               rectRainBar
+                  .transition()
+                  .duration( 500 )
+                  .attr( 'y', function( entry ) {
+                     return rainY( rain( entry ) );
+                  } )
+                  .attr( 'height', function( entry ) {
+                     return (contentBoxHeight - innerTopOffset) - rainY( rain( entry ) );
+                  } )
+                  .attr( 'title', function( entry ) {
+                     return formatString( messages.RAIN_IN_3_HOURS, {
+                        amount: formatDecimal( rain( entry ) )
+                     } );
+                  } );
+
+               // ENTER
+               rectRainBar.enter().append( 'svg:rect' )
                   .attr( 'class', 'rain-bar' )
                   .attr( 'x', function( entry, index ) {
                      return entryX( entry, index ) - rainBarWidth / 2;
@@ -190,6 +257,8 @@ define( [
                         $( bar ).tooltip( { container: 'body' } );
                      } );
                   } );
+
+               // EXIT
                rectRainBar.exit().remove();
             }
 
@@ -207,22 +276,22 @@ define( [
 
             //////////////////////////////////////////////////////////////////////////////////////////////////
 
+            function entryX( entry, index ) {
+               return timePeriodWidth / 2 + index * timePeriodWidth;
+            }
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+
             function lineX( weatherData, index ) {
                return index * timePeriodWidth;
             }
 
             //////////////////////////////////////////////////////////////////////////////////////////////////
 
-            function prop( propName ) {
+            function propertyAccessor( propName, fallback ) {
                return function( object ) {
-                  return object[ propName ];
+                  return ax.object.path( object, propName, fallback );
                };
-            }
-
-            //////////////////////////////////////////////////////////////////////////////////////////////////
-
-            function rain( weatherEntry ) {
-               return ax.object.path( weatherEntry, 'rain.3h', 0 );
             }
 
          }
